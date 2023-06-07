4160f3fcda0ba23596860c23457aefc8d396bd7d
Olga <korobkova.olja@gmail.com>
2023-06-07 12:31:36 +0300

    Improvement double click back to exit

--- app/src/main/java/com/example/myappathorization/MainActivity.kt
+++ app/src/main/java/com/example/myappathorization/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     var backPressedTime: Long = 0
+    private lateinit var backToast: Toast
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,10 +42,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onBackPressed(){
         if (backPressedTime + 2000 > System.currentTimeMillis() ){
+            backToast.cancel()
             super.onBackPressed()
             finish()
         } else{
-            Toast.makeText(this, "Нажмите еще раз, чтобы выйти", Toast.LENGTH_SHORT).show()
+            backToast = Toast.makeText(this, "Нажмите еще раз, чтобы выйти", Toast.LENGTH_SHORT)
+            backToast.show()
         }
         backPressedTime = System.currentTimeMillis()
     }
